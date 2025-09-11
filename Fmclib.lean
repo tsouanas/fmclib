251a9aef3ecfc4dd909428b209f0583670e6822c
Thanos Tsouanas <thanos@tsouanas.org>
2025-09-11 11:37:06 -0300

    reorg

--- Fmclib.lean
+++ Fmclib.lean
@@ -1,3 +1,6 @@
 -- This module serves as the root of the `Fmclib` library.
 -- Import modules here that should be built as part of the library.
-import Fmclib.Logic
+-- import Fmclib.IntroProof
+-- import Fmclib.RecInd
+-- import Fmclib.Integers
+-- import Fmclib.Reals
